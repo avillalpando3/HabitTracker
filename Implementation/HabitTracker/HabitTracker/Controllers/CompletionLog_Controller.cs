97ada38d8a9b0ff17567b606bf4e157f273b8d27
Adrian Villalpando <avillalpand3@gmail.com>
2021-02-07 17:59:58 -0600

    fixed typo in namespace declaration

--- Implementation/HabitTracker/HabitTracker/Controllers/CompletionLog_Controller.cs
+++ Implementation/HabitTracker/HabitTracker/Controllers/CompletionLog_Controller.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CompletionLogTracker.Controllers
+namespace HabitTracker.Controllers
 {
     public class CompletionLog_Controller : DBEntity
     {
