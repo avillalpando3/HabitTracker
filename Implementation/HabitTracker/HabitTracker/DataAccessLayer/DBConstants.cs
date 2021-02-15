081101ad0d0e8e09a64eb7e03fef377c0b1e86b7
Mehdi <gharibkhani@gmail.com>
2021-02-14 19:03:44 -0600

    Fix DBConstants

--- Implementation/HabitTracker/HabitTracker/DataAccessLayer/DBConstants.cs
+++ Implementation/HabitTracker/HabitTracker/DataAccessLayer/DBConstants.cs
@@ -7,25 +7,24 @@ namespace HabitTracker.DAL
     static class DBConstants
     {
 
-        // Database connection definition
-        private const string DatabaseFilename = "HabitTracker.db";
-        private static string basePath = $"{AppDomain.CurrentDomain.BaseDirectory}\\Database";
-        private static string DatabasePath = Path.Combine(basePath, DatabaseFilename);
+        public const string DatabaseFilename = "HabitTracker.db";
 
-        static readonly Lazy<SQLiteAsyncConnection> lazyInitializer = new Lazy<SQLiteAsyncConnection>(() =>
-        {
-            return new SQLiteAsyncConnection(DatabasePath, Flags);
-        });
-        internal static SQLiteAsyncConnection database => lazyInitializer.Value;
-        // End database connection definition
-
-        internal const SQLite.SQLiteOpenFlags Flags =
+        public const SQLite.SQLiteOpenFlags Flags =
             // open the database in read/write mode
             SQLite.SQLiteOpenFlags.ReadWrite |
             // create the database if it doesn't exist
             SQLite.SQLiteOpenFlags.Create |
             // enable multi-threaded database access
             SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath
+        {
+            get
+            {
+                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(basePath, DatabaseFilename);
+            }
+        }
 
     }
 }
