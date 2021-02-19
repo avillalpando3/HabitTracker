7decb3a5658c62a975b0a5dba98e257ba9bd0655
Joshua Kidder <jkidder5@my.devry.edu>
2021-02-18 21:36:19 -0500

    ItemBarMenu

--- Implementation/HabitTracker/HabitTracker/ViewModels/AboutViewModel.cs
+++ Implementation/HabitTracker/HabitTracker/ViewModels/AboutViewModel.cs
@@ -9,7 +9,7 @@ namespace HabitTracker.ViewModels
     {
         public AboutViewModel()
         {
-            Title = "About";
+            Title = "HabitTracker";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
         }
 
--- Implementation/HabitTracker/HabitTracker/Views/AboutPage.xaml.cs
+++ Implementation/HabitTracker/HabitTracker/Views/AboutPage.xaml.cs
@@ -3,6 +3,11 @@ using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+/*
+ * https//download.microsoft.com/download/7/8/8/788971A6-C4BB-43CA-91DC-557B8BE72928/
+ * Microsoft_Press_eBook_CreatingMobileAppswithXamarinForms_PDF.pdf
+*/
+
 namespace HabitTracker.Views
 {
     public partial class AboutPage : ContentPage
@@ -10,6 +15,11 @@ namespace HabitTracker.Views
         public AboutPage()
         {
             InitializeComponent();
+        }
+        void OnToolbarItemClicked(object sender, EventArgs args)
+        {
+            ToolbarItem toolbarItem = (ToolbarItem)sender;
+            label.Text = "ToolbarItem '" + toolbarItem.Text + "' clicked";
         }
     }
 }
