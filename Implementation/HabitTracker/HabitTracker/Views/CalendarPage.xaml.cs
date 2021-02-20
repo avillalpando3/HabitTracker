34cb059e8e79bba4691e44dc731c8b132236d466
Joshua Kidder <jkidder5@my.devry.edu>
2021-02-20 05:21:11 -0500

    CalendarView

--- Implementation/HabitTracker/HabitTracker/Views/CalendarPage.xaml.cs
+++ Implementation/HabitTracker/HabitTracker/Views/CalendarPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Syncfusion.SfCalendar.XForms;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,6 +17,24 @@ namespace HabitTracker.Views
         {
             Title = "Calendar";
             InitializeComponent();
+
+            // Adds the SfCalendar Control
+            SfCalendar calendar = new SfCalendar();
+            this.Content = calendar;
+
+            // Sets Blackout Dates
+            List<DateTime> black_dates = new List<DateTime>();
+            for (int i = 0; i < 5; i++)
+            {
+                DateTime date = new DateTime(2021, 1, 1 + i);
+                black_dates.Add(date);
+            }
+            calendar.BlackoutDates = black_dates;
+
+            // Restricts Dates to Specified Range
+            calendar.MinDate = new DateTime(2019, 1, 1);
+            calendar.MaxDate = new DateTime(2021, 12, 31);
+            this.Content = calendar;
         }
     }
 }
