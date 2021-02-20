930042d06a61eedc96a0ea78ba19358c4b7fd0c2
Joshua Kidder <jkidder5@my.devry.edu>
2021-02-19 23:28:17 -0500

    CalendarPage

--- Implementation/HabitTracker/HabitTracker/Views/AboutPage.xaml.cs
+++ Implementation/HabitTracker/HabitTracker/Views/AboutPage.xaml.cs
@@ -6,6 +6,9 @@ using Xamarin.Forms.Xaml;
 /*
  * https//download.microsoft.com/download/7/8/8/788971A6-C4BB-43CA-91DC-557B8BE72928/
  * Microsoft_Press_eBook_CreatingMobileAppswithXamarinForms_PDF.pdf
+ * 
+ * Pg 333: ToolBarItem
+ * Pg 920: Page Navigation
 */
 
 namespace HabitTracker.Views
@@ -16,10 +19,13 @@ namespace HabitTracker.Views
         {
             InitializeComponent();
         }
-        void OnToolbarItemClicked(object sender, EventArgs args)
+        async void OnToolbarItemClicked(object sender, EventArgs args)
         {
             ToolbarItem toolbarItem = (ToolbarItem)sender;
             label.Text = "ToolbarItem '" + toolbarItem.Text + "' clicked";
+
+            // Navigate to CalendarPage
+            await Navigation.PushAsync(new CalendarPage());
         }
     }
 }
